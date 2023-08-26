99e89651ed77bae0e16d80587305ca3f95dbed1a
bycyjs <704812528@qq.com>
2023-08-26 10:06:43 +0800

    704812528@qq.com

---
+++ bycyjs/src/currency/Weixing.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  <img src="../../public/image/contact/_-766393780__541d07d51d6d068cfb6404314ba82918_862400484_mmqrcode1691135145568_0_xg_0.jpg">
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
